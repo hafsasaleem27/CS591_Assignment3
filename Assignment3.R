6abb99cc618641d580fee9221a998fe5ae546086
Hafsa <hafsa.saleem27@gmail.com>
2025-11-11 01:47:47 +0500

    Initial commit: Added sakila tables and reading them using data.table

---
+++ Assignment3.R
@@ -0,0 +1,11 @@
+install.packages("data.table")
+library(data.table)
+
+film <- fread("data/film.csv")
+language <- fread("data/language.csv")
+customer <- fread("data/customer.csv")
+store <- fread("data/store.csv")
+staff <- fread("data/staff.csv")
+rental <- fread("data/rental.csv")
+inventory <- fread("data/inventory.csv")
+payment <- fread("data/payment.csv")
