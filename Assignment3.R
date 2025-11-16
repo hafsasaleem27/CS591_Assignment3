3c24f0db00fc1ee434e15ae4e61e8daaf5a4cd65
Hafsa <hafsa.saleem27@gmail.com>
2025-11-16 17:36:44 +0500

    Task 2: Calculated average rental rate of films, grouped by rating

--- Assignment3.R
+++ Assignment3.R
@@ -12,3 +12,7 @@ payment <- fread("data/payment.csv")
 
 pg_films <- film[rating == "PG" & rental_duration > 5]
 print(pg_films)
+
+avg_rental_rate <- film[, .(avg_rental_rate = mean(rental_rate, na.rm = TRUE)), by = rating]
+print(avg_rental_rate)
+
